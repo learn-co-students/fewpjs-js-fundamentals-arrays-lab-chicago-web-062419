9ff5e6e22693fd66e5a7bbd00c200e7e3f6ec011
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-08-05 09:28:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const  removeLast = ["Milo", "Otis", "Garfield"];
+const  removeFirst = ["Milo", "Otis", "Garfield"];
+const odie = "Odie";
+
+append.push(odie);
+prepend.unshift(odie)
+removeLast.pop()
+removeFirst.shift()
+
+console.log(append);
+console.log(prepend);
+console.log(removeLast);
+console.log(removeFirst)
